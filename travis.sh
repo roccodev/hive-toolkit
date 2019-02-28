d6c30b6a166b050d3d0738baa91e3edfcdf6c006
RoccoDev <roccodev.business@gmail.com>
2019-02-28 21:47:50 +0100

    Added domain mirroring

---
+++ travis.sh
@@ -0,0 +1,18 @@
+# Setup git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git clone --recursive https://${GIT_TOKEN}@github.com/RoccoDev/hive-toolkit.git # Clone the project into the BeezigLaby directory
+cd roccodev.github.io # cd into the directory
+
+# Remove the references to the submodules
+rm -rf .git
+
+rm CNAME
+echo "hive.rocco.dev" >> CNAME
+
+git init # Create a blank repository
+git remote add origin https://${GIT_TOKEN}@github.com/RoccoDev/HiveToolkit-Mirror.git # Add the mirror as remote
+git add --all # Add all the files
+git commit -m "Hive Toolkit mirror commit" # Commit changes
+git push origin master --force # Force push the repository
