276954b4fe75f6aa3f331d0e6e079f6c44985afe
RoccoDev <roccodev.business@gmail.com>
2019-08-19 11:30:30 +0200

    Added bedwars alerts

---
+++ bedwars/alerts.js
@@ -0,0 +1,25 @@
+let domAlert = document.getElementById("bed-alert")
+const url = "https://rocco.dev/archive/bedwars/alerts.json"
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+fetch(url, options).then(res => {
+    return res.json()
+}).then(json => {
+    if (!json || !json.length || json.length == 0) {
+        return
+    }
+    domAlert.hidden = false
+    let latest = json[0]
+    let text = latest.text
+    let link = latest.link
+
+    domAlert.innerHTML = text
+    domAlert.innerHTML += '<br><a href="' + link + '">Read more</a>'
+})
