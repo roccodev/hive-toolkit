a48f7ed35e89c1e83ba5554668bc6c4e7c71165f
RoccoDev <roccodev.business@gmail.com>
2019-08-19 11:39:51 +0200

    Use xhr for alerts

--- bedwars/alerts.js
+++ bedwars/alerts.js
@@ -1,24 +1,23 @@
 let domAlert = document.getElementById("bed-alert")
 const url = "https://rocco.dev/archive/bedwars/alerts.json"
-const options = {
-    method: 'GET',
-    cache: 'default',
-    headers: {
-        'Content-Type': 'application/json'
+
+var xhr = new XMLHttpRequest()
+xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            let json = JSON.parse(xhr.responseText)
+            if (!json || !json.length || json.length == 0) {
+                return
+            }
+            domAlert.hidden = false
+            let latest = json[0]
+            let text = latest.text
+            let link = latest.link
+
+            domAlert.innerHTML = text
+            domAlert.innerHTML += '<br><a href="' + link + '">Read more</a>'
+        }
     }
 }
-
-fetch(url, options).then(res => {
-    return res.json()
-}).then(json => {
-    if (!json || !json.length || json.length == 0) {
-        return
-    }
-    domAlert.hidden = false
-    let latest = json[0]
-    let text = latest.text
-    let link = latest.link
-
-    domAlert.innerHTML = text
-    domAlert.innerHTML += '<br><a href="' + link + '">Read more</a>'
-})
+xhr.open("GET", url, true)
+xhr.send(null)
